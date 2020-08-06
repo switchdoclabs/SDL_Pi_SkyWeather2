f02f9ea5fdc9dabd0cc0032d0ef942f5573c5e1e
SDL <you@example.com>
2020-08-06 09:25:03 -0700

    V006

--- sendemail.py
+++ sendemail.py
@@ -8,31 +8,31 @@ def sendEmail(source, message, subject, toaddress, fromaddress, filename):
         import config
 
 	# Import smtplib for the actual sending function
-	import smtplib
+        import smtplib
 
-	# Here are the email package modules we'll need
-	from email.mime.image import MIMEImage
-	from email.mime.multipart import MIMEMultipart
-	from email.mime.text import MIMEText
+        # Here are the email package modules we'll need
+        from email.mime.image import MIMEImage
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
 
-	COMMASPACE = ', '
+        COMMASPACE = ', '
 
 	# Create the container (outer) email message.
-	msg = MIMEMultipart()
-	msg['Subject'] = subject 
+        msg = MIMEMultipart()
+        msg['Subject'] = subject 
 	# me == the sender's email address
 	# family = the list of all recipients' email addresses
-	msg['From'] = fromaddress
-	msg['To'] =  toaddress
+        msg['From'] = fromaddress
+        msg['To'] =  toaddress
 	#msg.attach(message) 
 
-	mainbody = MIMEText(message, 'plain')
-	msg.attach(mainbody)
+        mainbody = MIMEText(message, 'plain')
+        msg.attach(mainbody)
 
 	# Assume we know that the image files are all in PNG format
     	# Open the files in binary mode.  Let the MIMEImage class automatically
        	# guess the specific image type.
-	if (filename != ""):
+        if (filename != ""):
                 fp = open(filename, 'rb')
                 img = MIMEImage(fp.read())
                 fp.close()
@@ -40,26 +40,26 @@ def sendEmail(source, message, subject, toaddress, fromaddress, filename):
 
 	# Send the email via our own SMTP server.
 
-	try:
-		# open up a line with the server
-		s = smtplib.SMTP("smtp.gmail.com", 587)
-		s.ehlo()
-		s.starttls()
-		s.ehlo()
+        try:
+            # open up a line with the server
+            s = smtplib.SMTP("smtp.gmail.com", 587)
+            s.ehlo()
+            s.starttls()
+            s.ehlo()
 
-		# login, send email, logout
-		s.login(config.mailUser, config.mailPassword)
-		s.sendmail(config.mailUser, toaddress, msg.as_string())
-		#s.close()
+            # login, send email, logout
+            s.login(config.mailUser, config.mailPassword)
+            s.sendmail(config.mailUser, toaddress, msg.as_string())
+            #s.close()
 
 
-		s.quit()
+            s.quit()
 
-	except:
-                if (config.SWDEBUG):
-                   pass
-                   #print (traceback.format_exc())
-                print("sendmail exception raised")
-	return 0
+        except:
+            if (config.SWDEBUG):
+                 pass
+                 #print (traceback.format_exc())
+                 print("sendmail exception raised")
+        return 0
 
 
